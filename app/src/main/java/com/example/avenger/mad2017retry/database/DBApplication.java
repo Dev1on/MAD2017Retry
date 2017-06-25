6150cea05d8977c22d91e08b5182d52396f8a630
AdrianStr <adrian.strzelczyk@stud.fh-luebeck.de>
2017-06-25 15:00:52 +0200

    more test data

--- app/src/main/java/com/example/avenger/mad2017retry/database/DBApplication.java
+++ app/src/main/java/com/example/avenger/mad2017retry/database/DBApplication.java
@@ -35,8 +35,23 @@ public class DBApplication extends Application  {
         }
 
         Todo newItem1 = new Todo("Name1", "Description1");
+        newItem1.setDone(false);
+        newItem1.setExpiry(123);
+        newItem1.setFavourite(false);
+        newItem1.setLocation(new Todo.Location("Havanna", new Todo.LatLng(2,3)));
+
         Todo newItem2 = new Todo("Name2", "Description2");
+        newItem2.setDone(true);
+        newItem2.setExpiry(1234);
+        newItem2.setFavourite(true);
+        newItem2.setLocation(new Todo.Location("Manhattan", new Todo.LatLng(2,3)));
+
         Todo newItem3 = new Todo("Name3", "Description3");
+        newItem3.setDone(false);
+        newItem3.setExpiry(1235);
+        newItem3.setFavourite(true);
+        newItem3.setLocation(new Todo.Location("Warschau", new Todo.LatLng(2,3)));
+
         crudOperations.createToDo(newItem1, (Todo result) -> Log.d(logger, "Item 1 created."));
         crudOperations.createToDo(newItem2, (Todo result) -> Log.d(logger, "Item 2 created."));
         crudOperations.createToDo(newItem3, (Todo result) -> Log.d(logger, "Item 3 created."));
