83daed3b2b387d4ada23743fe4c7da961a950288
AdrianStr <adrian.strzelczyk@stud.fh-luebeck.de>
2017-06-25 21:59:17 +0200

    added toolbar to detail

--- app/src/main/java/com/example/avenger/mad2017retry/ToDoDetailActivity.java
+++ app/src/main/java/com/example/avenger/mad2017retry/ToDoDetailActivity.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
@@ -28,6 +32,7 @@ public class ToDoDetailActivity extends AppCompatActivity implements ToDoDetailV
     private EditText locationText;
 
     private ProgressDialog progressDialog;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +49,11 @@ public class ToDoDetailActivity extends AppCompatActivity implements ToDoDetailV
         locationText = (EditText) findViewById(R.id.locationText);
 
         progressDialog = new ProgressDialog(this);
+        toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
+
+        setSupportActionBar(toolbar);
+
+
 
         initializeScreen();
     }
@@ -93,6 +103,22 @@ public class ToDoDetailActivity extends AppCompatActivity implements ToDoDetailV
     public void onDestroy() {
         presenter.onDestroy();
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail_menu_resource, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_delete) {
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 
 }
