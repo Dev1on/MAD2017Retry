b4a0762076ed9e7cc7af6c878ede188239a1d5d3
dev1on <respondek.kevin@gmail.com>
2017-06-25 17:05:58 +0200

    + added recyclerview for todolist (not working)

--- app/src/main/java/com/example/avenger/mad2017retry/ToDoListActivity.java
+++ app/src/main/java/com/example/avenger/mad2017retry/ToDoListActivity.java
@@ -1,42 +1,42 @@
 package com.example.avenger.mad2017retry;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.avenger.mad2017retry.presenter.ToDoListPresenter;
+import com.example.avenger.mad2017retry.view.ToDoListAdapter;
 import com.example.avenger.mad2017retry.view.ToDoListView;
 
 public class ToDoListActivity extends AppCompatActivity implements ToDoListView {
 
     private ToDoListPresenter presenter;
 
-    private ViewGroup todoListView;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager recyclerViewLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
 
-        presenter = new ToDoListPresenter(this);
-
-        todoListView = (ViewGroup) findViewById(R.id.toDoList);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerViewLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recyclerViewLayoutManager);
+
+        String[] myStrings = {"test", "test2", "bla","test", "test2", "bla","test", "test2", "bla"};
+        adapter = new ToDoListAdapter(myStrings);
+        recyclerView.setAdapter(adapter);
+
+        presenter = new ToDoListPresenter(this);
     }
 
     @Override
