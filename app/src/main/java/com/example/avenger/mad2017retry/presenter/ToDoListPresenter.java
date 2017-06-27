a16a5f6eb0d2190290490ae811f758415db276eb
dev1on <respondek.kevin@gmail.com>
2017-06-27 19:42:48 +0200

    + list view

--- app/src/main/java/com/example/avenger/mad2017retry/presenter/ToDoListPresenter.java
+++ app/src/main/java/com/example/avenger/mad2017retry/presenter/ToDoListPresenter.java
@@ -1,5 +1,8 @@
 package com.example.avenger.mad2017retry.presenter;
 
+import android.util.Log;
+
+import com.example.avenger.mad2017retry.database.ICRUDOperationsAsync;
 import com.example.avenger.mad2017retry.view.ToDoListView;
 
 /**
@@ -10,6 +13,8 @@ public class ToDoListPresenter {
 
     private ToDoListView toDoListView;
 
+    private ICRUDOperationsAsync crudOperations;
+
     public ToDoListPresenter(ToDoListView aToDoListView) {
         this.toDoListView = aToDoListView;
     }
@@ -17,4 +22,26 @@ public class ToDoListPresenter {
     public void onDestroy() {
         toDoListView = null;
     }
+/*
+    public void readToDos() {
+        //First of all let application check if there is a item with the given id.
+        //if yes read the item out of the map, if not then use the crudOperations to read from db
+
+        crudOperations.readAllToDos(result -> {
+            Log.i("ReadAllTodos", "Result is: " + result);
+
+            if(result.isEmpty()) {
+                toDoListView.displayTodosNotFound();
+            } else {
+                toDoListView.setToDoListView();
+            }
+
+            if(result.getId() == 0) {
+                toDoDetailView.displayTodoNotFound();
+            } else {
+                setTodo(result);
+                toDoDetailView.setTodoView(result);
+            }
+        });
+    }*/
 }
